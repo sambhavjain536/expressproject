d8d71e6388cd42738bfb17975ca333ac60569534
sambhavjain536! <sambhavjain896@gmail.com>
2020-01-07 12:44:37 +0530

    22 agust

--- controller/default.js
+++ controller/default.js
@@ -2,6 +2,7 @@ var express=require("express");
 var router=express.Router();
 
 router.use("/login",require("./login"));
+router.use("/signup",require("./signup"));
 
 
 
---
+++ controller/signup.js
@@ -0,0 +1,7 @@
+var express=require('express');
+var router=express.Router();
+router.get("/",function(req,res){
+    var pagedata ={"pagename":"signup","tittle":"signup page"}
+    res.render("layout",pagedata);
+    console.log("sign up is running ");
+})
